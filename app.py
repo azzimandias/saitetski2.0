0068fe014420615dbe77a66e9816f5e5530632d2
Azzimandias Shar <azzimandias@gmail.com>
2020-02-12 00:57:49 +0300

    почти все

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template, flash, redirect, url_for, request
 import logging
 from flask_login import LoginManager, logout_user, login_required, login_user, current_user
 from config import Config
-from forms import LoginForm, RegistrationForm, CheckForm, ExtendForm
+from forms import LoginForm, RegistrationForm, CheckForm, ExtendForm, Sub1, Sub2, Sub3, Sub4, Sub5, Sub6, Sub7, Sub8
 from flask_login import UserMixin
 #from __init__ import login
 from DB import cur, con
@@ -13,7 +13,8 @@ global a
 a = 0
 global passworld
 passworld = ''
-
+global card
+card = 0
 # Run ######################################################################
 app = Flask(__name__)
 
@@ -49,9 +50,142 @@ class User(UserMixin):
 
 
 # Routes ######################################################################
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def MainPage():
-    return render_template('MainPage.html')#, login=login)
+    f1 = Sub1()
+    f1d = Sub2()
+    f2 = Sub3()
+    f2d = Sub4()
+    f3 = Sub5()
+    f3d = Sub6()
+    f4 = Sub7()
+    f4d = Sub8()
+    if (request.method == 'POST'):
+        s = 0
+        print(request.form)
+        for rec in request.form:
+            if rec == 'submit1':
+                s = 1
+            if rec == 'submit1d':
+                s = 2
+            if rec == 'submit2':
+                s = 3
+            if rec == 'submit2d':
+                s = 4
+            if rec == 'submit3':
+                s = 5
+            if rec == 'submit3d':
+                s = 6
+            if rec == 'submit4':
+                s = 7
+            if rec == 'submit4d':
+                s = 8
+            bor = []
+            bor = sex(s)
+            print(bor)
+    cur.execute("select distinct * from exhibitions")
+    exhibitions = cur.fetchall()
+    j = []
+    for exx in exhibitions:
+        j.append(exx[0])
+
+    con.commit()
+    cur.execute("select distinct hall_name from hall")
+    hall_name = cur.fetchall()
+    i = []
+    for hall in hall_name:
+        i.append(hall[0])
+
+    con.commit()
+    cur.execute("select distinct floor№ from hall")
+    floor = cur.fetchall()
+    f = []
+    for ffloor in floor:
+        f.append(ffloor[0])
+
+    con.commit()
+    cur.execute("select distinct price from tickets")
+    price = cur.fetchall()
+    p = []
+    for pprice in price:
+        p.append(pprice[0])
+    con.commit()
+    return render_template('MainPage.html', exhibition1=j[0], exhibition2=j[1], exhibition3=j[2], exhibition4=j[3],
+                                            hall_name1=i[0], hall_name2=i[1], hall_name3=i[2], hall_name4=i[3],
+                                            floor1=f[0], floor2=f[1], floor3=f[2], floor4=f[3],
+                                            price1=p[1], price1d=p[0], price2=p[3], price2d=p[2], price3=p[5], price3d=p[4], price4=p[7], price4d=p[6],
+                                            f1=f1, f1d=f1d, f2=f2, f2d=f2d, f3=f3, f3d=f3d, f4=f4, f4d=f4d)
+
+
+def sex(s):
+    cur.execute("select distinct price from tickets")
+    tic = cur.fetchall()
+    for ttic in tic:
+        if s == 2:
+            price = ttic[0]
+        elif s == 1:
+            price = ttic[1]
+        elif s == 4:
+            price = ttic[2]
+        elif s == 3:
+            price = ttic[3]
+        elif s == 6:
+            price = ttic[4]
+        elif s == 5:
+            price = ttic[5]
+        elif s == 8:
+            price = ttic[6]
+        elif s == 7:
+            price = ttic[7]
+    con.commit()
+    cur.execute("select distinct ttype_name from tickets_types")
+    typ = cur.fetchall()
+    for ttyp in typ:
+        if s == 2:
+            type = ttyp[0]
+        if s == 1:
+            type = ttyp[1]
+        if s == 4:
+            type = ttyp[0]
+        if s == 3:
+            type = ttyp[1]
+        if s == 6:
+            type = ttyp[0]
+        if s == 5:
+            type = ttyp[1]
+        if s == 8:
+            type = ttyp[0]
+        if s == 7:
+            type = ttyp[1]
+    con.commit()
+    cur.execute("select distinct * from exhibitions")
+    exh = cur.fetchall()
+    for eexh in exh:
+        if s == 2:
+            exhibition = eexh[0]
+        if s == 1:
+            exhibition = eexh[0]
+        if s == 4:
+            exhibition = eexh[1]
+        if s == 3:
+            exhibition = eexh[1]
+        if s == 6:
+            exhibition = eexh[2]
+        if s == 5:
+            exhibition = eexh[2]
+        if s == 8:
+            exhibition = eexh[3]
+        if s == 7:
+            exhibition = eexh[3]
+    con.commit()
+    ticket = []
+    inside = {
+        'exhibition' : exhibition,
+        'type' : type,
+        'price' : price
+    }
+    ticket.append(inside)
+    return ticket
 
 
 @app.route('/SingUp', methods=['GET', 'POST'])
@@ -82,6 +216,7 @@ def Profile():
     year = 0
     CVV = 0
     extend = 0
+    # extend = 1
     if usn == '':
         return redirect(url_for('MainPage'))
     user = current_user.username
@@ -116,6 +251,7 @@ def Profile():
             cur.execute("ROLLBACK")
             con.commit()
     return render_template('Profile.html', form=form, extend=extend)
+    # return render_template('Profile.html', extend=extend, card=card, month=month, year=year, CVV=CVV)
 
 
 @app.route('/Loh', methods=['GET', 'POST'])
@@ -188,3 +324,7 @@ def Logout():
 def logout():
     logout_user()
     return redirect(url_for('SingIn'))
+
+@app.route('/Search')
+def Seach():
+    return render_template('Search.html')
--- forms.py
+++ forms.py
@@ -32,6 +32,33 @@ class ExtendForm(FlaskForm):
     # username = StringField('Имя пользователя', validators=[DataRequired()])
     # password = PasswordField('Пароль', validators=[DataRequired()])
 
+# class AllSubmits(FlaskForm):
+#     submit1 = SubmitField('Добавить в корзину')
+#     submit1d = SubmitField('Добавить в корзину')
+#     submit2 = SubmitField('Добавить в корзину')
+#     submit2d = SubmitField('Добавить в корзину')
+#     submit3 = SubmitField('Добавить в корзину')
+#     submit3d = SubmitField('Добавить в корзину')
+#     submit4 = SubmitField('Добавить в корзину')
+#     submit4d = SubmitField('Добавить в корзину')
+
+class Sub1(FlaskForm):
+    submit1 = SubmitField('Добавить в корзину')
+class Sub2(FlaskForm):
+    submit1d = SubmitField('Добавить в корзину')
+class Sub3(FlaskForm):
+    submit2 = SubmitField('Добавить в корзину')
+class Sub4(FlaskForm):
+    submit2d = SubmitField('Добавить в корзину')
+class Sub5(FlaskForm):
+    submit3 = SubmitField('Добавить в корзину')
+class Sub6(FlaskForm):
+    submit3d = SubmitField('Добавить в корзину')
+class Sub7(FlaskForm):
+    submit4 = SubmitField('Добавить в корзину')
+class Sub8(FlaskForm):
+    submit4d = SubmitField('Добавить в корзину')
+
     def validate_username(self, username):
         cur.execute("SELECT login FROM customers")
         loginList = cur.fetchall()
@@ -39,3 +66,4 @@ class ExtendForm(FlaskForm):
             if username.data == login[0]:
                 raise ValidationError('Это имя пользователя занято, придумайте другое.')
                 raise ValidationError('Please use a different username.')
+
